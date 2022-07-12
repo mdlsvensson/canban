0342604a85e48a58eb72322a4e4146b1e55c3fbe
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-12 12:04:20 +0200

    Create AddProjectButton.svelte

---
+++ src/lib/projectbar/AddProjectButton.svelte
@@ -0,0 +1,17 @@
+<button
+	class="w-8 h-8 bg-white rounded-xl focus:outline-none flex items-center justify-center hover:scale-105"
+>
+	<svg
+		class="h-4 fill-gray-900 transition"
+		clip-rule="evenodd"
+		fill-rule="evenodd"
+		stroke-linejoin="round"
+		stroke-miterlimit="2"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			d="m11 11h-7.25c-.414 0-.75.336-.75.75s.336.75.75.75h7.25v7.25c0 .414.336.75.75.75s.75-.336.75-.75v-7.25h7.25c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-7.25v-7.25c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+			fill-rule="nonzero"
+		/></svg
+	>
+</button>

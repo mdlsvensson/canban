41544e2fb2aa066a10b03bf2102f2a043c02a443
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-12 21:48:45 +0200

    .gitignore changed

--- src/lib/projectbar/PorjectButton.svelte
+++ src/lib/projectbar/PorjectButton.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
-	//ring-2 ring-offset-2 ring-offset-gray-600 ring-white
+	import { page } from '$app/stores';
+
+	export let project: string = 'test';
+
+	function setUrl() {
+		$page.url.pathname = project;
+	}
 </script>
 
-<button class="w-8 h-8 bg-white rounded-xl focus:outline-none hover:scale-105" />
+<button
+	class="w-8 h-8 bg-white rounded-xl focus:outline-none hover:scale-105"
+	class:active={$page.url.pathname === project}
+	on:click={setUrl}
+/>
+
+<style>
+	.active {
+		@apply ring-2;
+		@apply ring-offset-2;
+		@apply ring-offset-gray-600;
+		@apply ring-white;
+	}
+</style>

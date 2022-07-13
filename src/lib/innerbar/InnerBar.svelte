b8ff1cd6d3bebbab062d922bb767c07e94f27a8f
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-13 21:33:04 +0200

    Create InnerBar.svelte

---
+++ src/lib/innerbar/InnerBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Search from './Search.svelte';
+</script>
+
+<div
+	class="grow h-16 flex justify-center items-center justify-between border-r border-b border-black border-solid p-6"
+>
+	<div class="flex gap-2">
+		<strong><h1>Project Name</h1></strong>
+		<h3>Board</h3>
+	</div>
+	<div class="flex gap-2">
+		<Search />
+	</div>
+</div>

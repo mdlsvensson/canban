79ef0363b808bd656d93c5c65fb1901b8eb484d4
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-13 23:39:54 +0200

    Added close on click outside

--- src/lib/common/Modal.svelte
+++ src/lib/common/Modal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { clickOutside } from '$lib/util/clickOutside';
+
 	export let isOpen = false;
 
 	const onKeyDown = (event: KeyboardEvent) => {
@@ -12,12 +14,20 @@
 		if (isOpen) return;
 		isOpen = !isOpen;
 	};
+
+	const handleClickOutside = () => {
+		if (isOpen) {
+			isOpen = !isOpen;
+		}
+	};
 </script>
 
 <div
 	class="z-50 absolute w-[20rem] bg-gray-300 top-[5rem] right-[1rem] rounded-lg content-none after:absolute after:top-0 after:right-6 after:w-14 after:h-14 after:rotate-45 after:bg-gray-300 animate-fade after:z-[-1]"
 	class:invisible={!isOpen}
 	on:click|stopPropagation={() => {}}
+	use:clickOutside
+	on:click_outside={handleClickOutside}
 >
 	<slot />
 </div>

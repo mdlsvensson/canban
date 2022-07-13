bdc4c3e55b65a329ed12ec272a1b98b72d0de580
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-13 15:25:52 +0200

    Added InnerBar and UserBar components.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import ProjectBar from '$lib/projectbar/ProjectBar.svelte';
+	import InnerBar from '$lib/innerbar/InnerBar.svelte';
+	import UserBar from '$lib/userbar/UserBar.svelte';
+
+	let displayedProjects: object;
 </script>
 
 <div class="w-full h-screen flex">
 	<ProjectBar />
 	<div class="flex-grow">
-		<div class="w-full h-16 bg-gray-200" />
+		<div class="flex bg-gray-200">
+			<InnerBar />
+			<UserBar />
+			<div />
+		</div>
 		<slot />
 	</div>
 </div>

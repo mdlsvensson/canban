27d956ed1c7b04e140699a47f5789aa8ffb66dfd
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-13 21:33:45 +0200

    Create SignInButton.svelte

---
+++ src/lib/userbar/SignInButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Modal from '$lib/common/Modal.svelte';
+	import Login from '$lib/auth/Login.svelte';
+
+	let isOpen = false;
+</script>
+
+<button
+	type="button"
+	class="relative bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+	on:click={() => {
+		isOpen = !isOpen;
+	}}
+>
+	Sign in
+</button>
+
+<Modal bind:isOpen>
+	<Login />
+</Modal>

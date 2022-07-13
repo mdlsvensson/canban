8b5ac864dfbc5d314d0a20d300c2e9e4c533236e
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-13 22:01:37 +0200

    Size adjustment

--- src/lib/common/Modal.svelte
+++ src/lib/common/Modal.svelte
@@ -21,17 +21,9 @@
 </script>
 
 <div
-	class="z-50 absolute h-[10rem] w-[20rem] bg-gray-300 top-[5rem] right-[1rem] rounded-lg content-none after:absolute after:top-0 after:right-6 after:w-14 after:h-14 after:rotate-45 after:bg-gray-300 animate-fade"
+	class="z-50 absolute w-[20rem] bg-gray-300 top-[5rem] right-[1rem] rounded-lg content-none after:absolute after:top-0 after:right-6 after:w-14 after:h-14 after:rotate-45 after:bg-gray-300 animate-fade after:z-[-1]"
 	class:invisible={!isOpen}
+	on:click|stopPropagation={() => {}}
 >
-	<div on:click|stopPropagation={() => {}}>
-		<!-- <svg on:click={close} viewBox="0 0 12 12">
-			<circle cx="6" cy="6" r="6" />
-			<line x1="3" y1="3" x2="9" y2="9" />
-			<line x1="9" y1="3" x2="3" y2="9" />
-		</svg> -->
-		<div>
-			<slot />
-		</div>
-	</div>
+	<slot />
 </div>

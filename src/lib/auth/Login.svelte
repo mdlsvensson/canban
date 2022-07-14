b231354f5e86a3a73ee3a54d12beaa95e0604dd3
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-14 11:04:33 +0200

    Added full-width to fix bug

--- src/lib/auth/Login.svelte
+++ src/lib/auth/Login.svelte
@@ -30,7 +30,7 @@
 						type="email"
 						autocomplete="email"
 						required
-						class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+						class="appearance-none w-full rounded-md px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 						placeholder="Email address"
 						bind:value={email}
 					/>

578ef8e3791b20baaccdba264de92377cbb4f050
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-15 00:25:27 +0200

    Applied theme, imported fonts, bug fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import '../fonts.css';
 	import '../app.css';
 	import ProjectBar from '$lib/projectbar/ProjectBar.svelte';
 	import InnerBar from '$lib/innerbar/InnerBar.svelte';
 	import UserBar from '$lib/userbar/UserBar.svelte';
+	import SideBar from '$lib/sidebar/SideBar.svelte';
 
 	import { user } from '$lib/stores';
 	import { supabase } from '$lib/supabaseClient';
@@ -14,14 +16,19 @@
 	});
 </script>
 
-<div class="w-full h-screen flex">
+<div class="w-full h-screen flex bg-bg">
 	<ProjectBar />
-	<div class="flex-grow">
-		<div class="flex bg-gray-200">
+	<div class="flex flex-grow flex-col">
+		<div class="flex">
 			<InnerBar />
 			<UserBar />
 			<div />
 		</div>
-		<slot />
+		<div class="flex flex-grow">
+			<SideBar />
+			<div class="w-full bg-bg1 p-8 shadow-inner">
+				<slot />
+			</div>
+		</div>
 	</div>
 </div>

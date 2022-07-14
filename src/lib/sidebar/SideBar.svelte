d7105004df4f25a9916b020bedee5e47f0c9b333
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-15 00:24:43 +0200

    Create SideBar.svelte

---
+++ src/lib/sidebar/SideBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import NavLink from './NavLink.svelte';
+	import Icon from '$lib/common/Icon.svelte';
+</script>
+
+<div class="h-full min-w-[12rem] flex flex-col p-4 gap-2 border-r border-bg2">
+	<NavLink href="/">
+		<div>
+			<Icon name="board" height="1.5rem" width="1.5rem" />
+		</div>
+		<p class="pt-[4px]">Board</p>
+	</NavLink>
+	<NavLink href="/profile">
+		<div>
+			<Icon name="settings" height="1.4rem" width="1.4rem" />
+		</div>
+		<p class="pt-[4px]">Settings</p>
+	</NavLink>
+</div>

76275ad7316870b3998a1ba3ecf65cc8da5a702d
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-15 00:24:37 +0200

    Create NavLink.svelte

---
+++ src/lib/sidebar/NavLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string;
+</script>
+
+<a
+	sveltekit:prefetch
+	{href}
+	class="px-4 py-3 hover:bg-bg1 rounded-md flex gap-2 items-center"
+	class:active-nav={$page.url.pathname === href}
+	class:font-bold={$page.url.pathname === href}
+>
+	<slot />
+</a>

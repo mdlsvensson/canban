51d3b1e62357d4e64661e87194aa9d5da1e5c766
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-07-19 10:38:36 +0200

    Applied theme and adjusted css

--- src/lib/auth/Login.svelte
+++ src/lib/auth/Login.svelte
@@ -30,7 +30,7 @@
 						type="email"
 						autocomplete="email"
 						required
-						class="appearance-none w-full rounded-md px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+						class="p-2 pl-4 w-full text-sm text-fg4 placeholder:text-fg4 bg-bg2 rounded-lg border border-bg3 focus:outline-none focus:border-fg4 shadow aling-middle pt-3"
 						placeholder="Email address"
 						bind:value={email}
 					/>
@@ -40,26 +40,10 @@
 			<div>
 				<button
 					type="submit"
-					class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+					class="p-3 w-full text-sm bg-bg2 rounded-lg border border-bg3 focus:outline-none focus:border-fg4 shadow pt-4 hover:border-fg4"
 					value={loading ? 'Loading' : 'Send magic link'}
 					disabled={loading}
 				>
-					<span class="absolute left-0 inset-y-0 flex items-center pl-3">
-						<!-- Heroicon name: solid/lock-closed -->
-						<svg
-							class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</span>
 					Sign in
 				</button>
 			</div>
